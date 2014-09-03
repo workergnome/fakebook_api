4bda3802e29e57736cb3dc7f9258a263d3d7db62
David Newbury <david.newbury@gmail.com>
2014-09-03 14:11:32 -0400

    Minor documentation tweaks

--- lib/background_task.rb
+++ lib/background_task.rb
@@ -1,10 +1,11 @@
+## This is the absolute minimum program for running a background worker.
+
 $stdout.sync = true # for foreman logging
 
 require "backburner"
 require_relative "jobs.rb"
 
 Backburner.configure do |config|
-  config.tube_namespace   = "fakebook"
+  config.tube_namespace = "fakebook"
 end
-
 Backburner.work
